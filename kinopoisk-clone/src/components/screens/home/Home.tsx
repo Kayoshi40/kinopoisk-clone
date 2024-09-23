c935461c20139909d5ba06032196b9cd40828c65
Kayoshi40 <savochkin2002rus@yandex.ru>
2024-09-24 02:39:01 +0300

    Без изменений

---
+++ kinopoisk-clone/src/components/screens/home/Home.tsx
@@ -0,0 +1,23 @@
+import { useActions } from '@/hooks/useActions'
+import { useAuth } from '@/hooks/useAuth'
+import { TypePaginationProducts } from '@/types/product.interface'
+import CatalogPagination from '@/ui/catalog/CatalogPagination'
+import Layout from '@/ui/layout/Layout'
+
+import Meta from '@/ui/meta'
+import { FC } from 'react'
+
+const Home: FC<TypePaginationProducts> = ({ products, length }) => {
+	const { user } = useAuth()
+	const { logout } = useActions()
+
+	return (
+		<Meta title='Home'>
+			<Layout>
+				<CatalogPagination title='Freshed products' data={{products, length}} />
+			</Layout>
+		</Meta>
+	)
+}
+
+export default Home
